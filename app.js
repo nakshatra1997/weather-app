966f7ad1accd96d0fab6b3b13214e61015d3410b
nakshatra1997 <nakshatrapradhan2013@gmail.com>
2017-11-09 23:59:40 +0530

    separating the code from app.js

--- app.js
+++ app.js
@@ -1,7 +1,35 @@
 const request=require('request');
+const yargs=require('yargs');
+
+const geocode=require('./geocode/geocode.js');//you can omit .js extension
+const argv=yargs
+   .options({
+               a:{
+               	demand:true,
+               	alias:'address',
+               	describe:'address to fetch weather',
+               	string:true
+               }
+            })
+   .help()
+   .alias('help','h')
+   .argv;
+geocode.getcodeAddress(argv.address);
 request({
-	url:'http://maps.googleapis.com/maps/api/geocode/json?address=Ghaziabad',
+	url:`http://maps.googleapis.com/maps/api/geocode/json?address=${argv.address}`,
 	json:true
 },(error,response,body)=>{
-	console.log(`latitude is ${body.results[0].geometry.location.lat} and longitude is ${body.results[0].geometry.location.lng}`);
+	if(error)
+	{
+		console.log('unable to connect google server');
+	}
+	else if(body.status=='ZERO_RESULTS')
+	{
+		console.log('unable to find the address');
+	}
+	else
+	{
+	   console.log(`latitude is ${body.results[0].geometry.location.lat} and longitude is ${body.results[0].geometry.location.lng}`);	
+	}
+	
 });
