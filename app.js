9909100245969827440e1987d66a57c1308e865a
nakshatra1997 <nakshatrapradhan2013@gmail.com>
2017-11-10 00:19:10 +0530

    added callback function to geocodeAddress to handle error message and data

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const request=require('request');
+
 const yargs=require('yargs');
 
 const geocode=require('./geocode/geocode.js');//you can omit .js extension
@@ -14,22 +14,15 @@ const argv=yargs
    .help()
    .alias('help','h')
    .argv;
-geocode.getcodeAddress(argv.address);
-request({
-	url:`http://maps.googleapis.com/maps/api/geocode/json?address=${argv.address}`,
-	json:true
-},(error,response,body)=>{
-	if(error)
+geocode.getcodeAddress(argv.address,(errorMessage,results)=>
 	{
-		console.log('unable to connect google server');
+       if(errorMessage)
+       {
+       	console.log(errorMessage);
+       }
+       else
+       {
+       	console.log(JSON.stringify(results,undefined,2));
+       }
 	}
-	else if(body.status=='ZERO_RESULTS')
-	{
-		console.log('unable to find the address');
-	}
-	else
-	{
-	   console.log(`latitude is ${body.results[0].geometry.location.lat} and longitude is ${body.results[0].geometry.location.lng}`);	
-	}
-	
 });
---
+++ geocode/geocode.js
@@ -0,0 +1,30 @@
+const request=require('request');
+
+var geocodeAddress=(address,callback)=>
+{
+	var encodedAddress=encodedURIComponent(address);
+  request({
+	url:`http://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
+	json:true
+},(error,response,body)=>{
+	if(error)
+	{
+		callback('unable to connect google server');
+	}
+	else if(body.status=='ZERO_RESULTS')
+	{
+		callback('unable to find the address');
+	}
+	else
+	{
+	   // callback(`latitude is ${body.results[0].geometry.location.lat} and longitude is ${body.results[0].geometry.location.lng}`);	
+	   callback(undefined,{
+	   	address:body.results[0].formatted_address,
+	   	latitude:body.results[0].geometry.location.lat,
+	   	longitude:body.results[0].geometry.location.lng
+	   });
+	}
+	
+});
+};
+module.exports.geocodeAddress=geocodeAddress;
